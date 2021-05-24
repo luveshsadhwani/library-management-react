99d12cf7a1f18d38c861b1df9313457ddfbf0413
Luvesh L Sadhwani <luveshsadhwani@gmail.com>
2021-05-24 19:10:56 +0800

    Added AppNavigator

---
+++ src/components/navigator/AppNavigator.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Switch, Route, useRouteMatch } from 'react-router-dom';
+
+import Home from '../../container/Home'
+import Profile from '../../container/Profile'
+import Dashboard from '../../container/Dashboard'
+import Events from '../../container/Events'
+import Location from '../../container/Location'
+
+export default function AppNavigator() {
+    let match = useRouteMatch().url;
+    console.log(match);
+    
+    return (
+        <Switch>
+            <Route path={`${match}`} component={Home} exact/>
+            <Route path={`${match}/profile`} component={Profile} exact/>
+            <Route path={`${match}/dashboard`} component={Dashboard} exact/>
+            <Route path={`${match}/events`} component={Events} exact/>
+            <Route path={`${match}/location`} component={Location} exact/>
+        </Switch>
+    )
+}
---
+++ src/container/Events.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Events() {
+    return(
+        <div>
+            This is the events page.
+        </div>
+    )
+}
