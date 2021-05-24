121f541fd8fce0f5f39b885ea64c53ef59b4674a
Ali™ <76595455+Ali-TM-original@users.noreply.github.com>
2021-05-24 19:17:02 +0500

    Added map unfinished with our locations

--- src/container/Location.jsx
+++ src/container/Location.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React,{ useState } from 'react'
+import ReactMapGl, {Marker} from "react-map-gl"
+import marker from "./assets/marker4.png"
 
 export default function Location() {
+
+    const[config,setConfig] = useState({
+        latutude:100,
+        longitude:100,
+        width:"100%",
+        height:"100vh",
+        zoom:5
+    });
+
     return(
-        <div>
-            This is the location page.
-        </div>
+        <ReactMapGl {...config}
+         maxZoom={20}
+        mapboxApiAccessToken={'Token From MapBox'}
+        onViewportChange={(newViewport)=>{
+            setConfig({...newViewport})
+        }}>
+            <Marker latitude={30.3753} longitude={69.3451}>
+                
+                    <img src={marker} color={'red'} alt="marker"></img>
+                
+            </Marker>
+            <Marker latitude={4.2105} longitude={101.9758}>
+                <img src={marker} alt="marker"></img>
+            </Marker>
+        </ReactMapGl>
+        
     )
 }
