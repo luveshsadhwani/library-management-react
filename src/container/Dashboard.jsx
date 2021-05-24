67d972c85d2e78d82894a95f6eb1eb7bc9a3e72b
Ali™ <76595455+Ali-TM-original@users.noreply.github.com>
2021-05-24 19:16:21 +0500

    Update Dashboard.jsx

--- src/container/Dashboard.jsx
+++ src/container/Dashboard.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from "axios"
+import {Link} from "react-router-dom"
+import "./customcss/dashboard.css"
 
 export default function Dashboard() {
+    const [users, setUsers] = useState([])
+
+    useEffect(()=>{
+        loaddata()
+    },[])
+
+    const loaddata = async()=>{
+        const resultdata = await axios.get("http://localhost:8000/users")
+        setUsers(resultdata.data)
+    }
+
+    console.log(users)
+
     return(
-        <div>
-            This is the dashboard page.
-        </div>
+        <div className="py-4 mr2 ml2">
+            <div>
+                <h1 className="center">Welcome to Dashboard</h1>
+            </div>
+            <div>
+                <button className="btn btn-outline-primary">Add Entry</button>
+            </div>
+            <table className="table table-light table-striped border shadow mt20">
+                <thead className="table table-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">UserName</th>
+                        <th scope="col">Email</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        users.map((user, index)=>(
+                            <tr>
+                                <th scope="row">{index+1}</th>
+                                <td>{user.name}</td>
+                                <td>{user.username}</td>
+                                <td>{user.email}</td>
+                                <td>
+                                    <Link className="btn btn-primary mr2">View</Link>
+                                    <Link className="btn btn-outline-primary mr2">Edit</Link>
+                                    <Link className="btn btn-danger mr2">Delete</Link>
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>  
     )
 }
