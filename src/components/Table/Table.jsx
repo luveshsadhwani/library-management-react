5ec04a0b18bc83fc7af2d11cbd76447efd582fa5
Ali™ <76595455+Ali-TM-original@users.noreply.github.com>
2021-05-31 09:58:24 +0500

    Removed unused import

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -6,7 +6,6 @@ import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
-import TableFooter from "@material-ui/core/TableFooter";
 import CustomTablePagination from "./TablePagination";
 
 import Paper from "@material-ui/core/Paper";
