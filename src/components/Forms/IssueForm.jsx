8b9f7fd8f288f73a8760c3876b4f485990910719
Luvesh L Sadhwani <luveshsadhwani@gmail.com>
2021-06-02 23:14:57 +0800

    add form to issue book

---
+++ src/components/Forms/IssueForm.jsx
@@ -0,0 +1,139 @@
+import React, { useState, useEffect } from "react";
+import { createStandaloneToast } from "@chakra-ui/react";
+import axios from "axios";
+import { useParams, useHistory } from "react-router-dom";
+import "./form.css";
+
+// This is where we have out input form
+
+export default function IssueForm() {
+  const toast = createStandaloneToast();
+  const [bookname, setBookname] = useState("");
+  const [author, setAuthor] = useState("");
+  const [isbnnumber, setIsbnumber] = useState("");
+  const [publisher, setPublisher] = useState("");
+  const [subject, setSubject] = useState("");
+  const [studentId, setStudentId] = useState("#XXXXXXXX");
+  const { id } = useParams();
+
+  let history = useHistory();
+
+  const submit_issue = async (e) => {
+    e.preventDefault();
+
+    //// CONNECTION TO THE CUSTOM API GOES HERE USE AXIOS TO SENT POST REQ
+    let datatosend = {
+      studentId,
+    };
+
+    console.log(datatosend);
+
+    // UPDATE WHERE ID
+    await axios
+      .post(`http://localhost:8000/issued`, null, {
+        params: {
+          index: id,
+          isseud: studentId,
+        },
+      })
+      .then(() => {
+        toast({
+          title: "Successfully Issued",
+          description: `Successfully Issued book to ${studentId}`,
+          status: "success",
+          variant: "solid",
+          duration: 1500,
+          position: "top-right",
+          isClosable: false,
+        });
+      })
+      .catch((err) => {
+        toast({
+          title: "Error Pushing Data",
+          description: `Error: ${err}`,
+          status: "error",
+          variant: "solid",
+          duration: 1500,
+          position: "top-right",
+          isClosable: false,
+        });
+      })
+      .finally(history.push("/home/dashboard"));
+  };
+
+  useEffect(() => {
+    async function retrieve() {
+      await axios
+        .get(`http://localhost:8000/find`, {params:{
+            entry_id: id
+        }})
+        .then((response) => {
+          setBookname(response.data.booktitle);
+          setAuthor(response.data.authorname);
+          setIsbnumber(response.data.Isbn);
+          setPublisher(response.data.Publisher);
+          setSubject(response.data.Subject);
+        })
+        .catch((err) => {
+          toast({
+            title: "Error Getting Data",
+            description: `Error: ${err}`,
+            status: "error",
+            variant: "solid",
+            duration: 1500,
+            position: "top-right",
+            isClosable: false,
+          });
+        });
+    }
+
+    retrieve();
+  }, [id, toast]);
+
+  return (
+    <div>
+      <div className="entry">
+        <div className="entrycontainer">
+          <div className="title">Issue Book</div>
+          <form onSubmit={submit_issue}>
+            <div className="entry-details">
+              <div className="entry-box">
+                <span className="details">Book Name</span>
+                <input value={bookname} disabled />
+              </div>
+              <div className="entry-box">
+                <span className="details">Author</span>
+                <input value={author} disabled />
+              </div>
+              <div className="entry-box">
+                <span className="details">Isbn Number</span>
+                <input value={isbnnumber} disabled />
+              </div>
+              <div className="entry-box">
+                <span className="details">Publisher</span>
+                <input value={publisher} disabled />
+              </div>
+              <div className="entry-box">
+                <span className="details">Subject</span>
+                <input value={subject} disabled />
+              </div>
+              <div className="entry-box">
+                <span className="details">Student ID</span>
+                <input
+                  value={studentId}
+                  onChange={(e) => {
+                    setStudentId(e.target.value);
+                  }}
+                  required
+                />
+              </div>
+            </div>
+            <div className="button">
+              <input type="submit" value="Issue Book" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/navigator/AppNavigator.jsx
+++ src/components/navigator/AppNavigator.jsx
@@ -7,8 +7,9 @@ import Dashboard from '../../container/Dashboard'
 import Issues from '../../container/Issues'
 import Location from '../../container/Location'
 import AddEntry from '../../container/functionalitycomp/AddEntry'
-import Edit from "../Edit/Edit"
+import Edit from "../Forms/Edit"
 import View from "../View/View"
+import IssueForm from "../Forms/IssueForm"
 
 export default function AppNavigator() {
     let match = useRouteMatch().url;
@@ -23,6 +24,7 @@ export default function AppNavigator() {
             <Route path={`${match}/add`} component={AddEntry} exact/>
             <Route path={`${match}/edit/:id`} component={Edit} exact/>
             <Route path={`${match}/view/:isbn`} component={View} exact/>
+            <Route path={`${match}/issue/:id`} component={IssueForm} exact/>
         </Switch>
     )
 }
