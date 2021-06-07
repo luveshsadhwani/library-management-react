6a54ebb69d9d878269f583714b7c784acfa14e9a
Luvesh L Sadhwani <luveshsadhwani@gmail.com>
2021-06-07 21:07:57 +0800

    rewritten code for add, issue, edit books

--- src/components/Forms/IssueForm.jsx
+++ src/components/Forms/IssueForm.jsx
@@ -7,47 +7,60 @@ import "./form.css";
 // This is where we have out input form
 
 export default function IssueForm() {
-  const toast = createStandaloneToast();
-  const [bookname, setBookname] = useState("");
-  const [author, setAuthor] = useState("");
-  const [isbnnumber, setIsbnumber] = useState("");
-  const [publisher, setPublisher] = useState("");
-  const [subject, setSubject] = useState("");
-  const [studentId, setStudentId] = useState("#XXXXXXXX");
-  const { id } = useParams();
+  const defaultBookInfoState = {
+    id: -1,
+    authorname: "",
+    booktitle: "",
+    Subject: "",
+    Publisher: "",
+    Isbn: "",
+    issued: "",
+  };
 
+  const [bookInfo, setBookInfo] = useState(defaultBookInfoState);
+  const toast = createStandaloneToast();
+  const { id } = useParams();
+  console.log(bookInfo);
   let history = useHistory();
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setBookInfo((prev) => ({
+      ...prev,
+      [e.target.name]: value,
+    }));
+  };
 
   const submit_issue = async (e) => {
     e.preventDefault();
 
     //// CONNECTION TO THE CUSTOM API GOES HERE USE AXIOS TO SENT POST REQ
-    let datatosend = {
-      studentId,
-    };
-
-    console.log(datatosend);
+    let params = bookInfo.issued;
+    console.log(params);
 
     // UPDATE WHERE ID
-    await axios.post(`http://localhost:8000/updateissued`, null, { params: {
-      entry_id:id,
-      data_to_push:studentId
-
-      }})
+    await axios
+      .post(`http://localhost:8000/updateissued`, null, {
+        params: {
+          entry_id: id,
+          data_to_push: params,
+        },
+      })
       .then(() => {
         toast({
           title: "Successfully Issued",
-          description: `Successfully Issued book to ${studentId}`,
+          description: `Successfully Issued book to ${params}`,
           status: "success",
           variant: "solid",
           duration: 1500,
           position: "top-right",
           isClosable: false,
         });
+        history.push("/home/dashboard");
       })
       .catch((err) => {
         toast({
-          title: "Error Pushing Data",
+          title: "Error Issuing Book",
           description: `Error: ${err}`,
           status: "error",
           variant: "solid",
@@ -55,22 +68,19 @@ export default function IssueForm() {
           position: "top-right",
           isClosable: false,
         });
-      })
-      .finally(history.push("/home/dashboard"));
+      });
   };
 
   useEffect(() => {
     async function retrieve() {
       await axios
-        .get(`http://localhost:8000/find`, {params:{
-            entry_id: id
-        }})
+        .get(`http://localhost:8000/find`, {
+          params: {
+            entry_id: id,
+          },
+        })
         .then((response) => {
-          setBookname(response.data.booktitle);
-          setAuthor(response.data.authorname);
-          setIsbnumber(response.data.Isbn);
-          setPublisher(response.data.Publisher);
-          setSubject(response.data.Subject);
+          setBookInfo(response.data);
         })
         .catch((err) => {
           toast({
@@ -86,7 +96,7 @@ export default function IssueForm() {
     }
 
     retrieve();
-  }, [id, toast]);
+  }, [id]);
 
   return (
     <div>
@@ -97,31 +107,31 @@ export default function IssueForm() {
             <div className="entry-details">
               <div className="entry-box">
                 <span className="details">Book Name</span>
-                <input value={bookname} disabled />
+                <input value={bookInfo.booktitle} disabled />
               </div>
               <div className="entry-box">
                 <span className="details">Author</span>
-                <input value={author} disabled />
+                <input value={bookInfo.authorname} disabled />
               </div>
               <div className="entry-box">
                 <span className="details">Isbn Number</span>
-                <input value={isbnnumber} disabled />
+                <input value={bookInfo.authorname} disabled />
               </div>
               <div className="entry-box">
                 <span className="details">Publisher</span>
-                <input value={publisher} disabled />
+                <input value={bookInfo.Publisher} disabled />
               </div>
               <div className="entry-box">
                 <span className="details">Subject</span>
-                <input value={subject} disabled />
+                <input value={bookInfo.Subject} disabled />
               </div>
               <div className="entry-box">
                 <span className="details">Student ID</span>
                 <input
-                  value={studentId}
-                  onChange={(e) => {
-                    setStudentId(e.target.value);
-                  }}
+                  name={Object.keys(defaultBookInfoState)[6]}
+                  value={bookInfo.issued}
+                  placeholder="#XXXXXXXX"
+                  onChange={handleChange}
                   required
                 />
               </div>
--- src/container/functionalitycomp/AddEntry.jsx
+++ src/container/functionalitycomp/AddEntry.jsx
@@ -1,111 +1,141 @@
-import React, {useState}  from 'react'
-import "./addentry.css"
-import { createStandaloneToast} from "@chakra-ui/react"
-import axios from "axios"
+import React, { useState } from "react";
+import "./addentry.css";
+import { createStandaloneToast } from "@chakra-ui/react";
+import axios from "axios";
 
-// This is where we have out input form 
-
-
+// This is where we have out input form
 
 export default function AddEntry() {
+  const defaultBookInfoState = {
+    id: -1,
+    authorname: "",
+    booktitle: "",
+    Subject: "",
+    Publisher: "",
+    Isbn: "",
+    issued: "",
+  };
 
-    const toast =  createStandaloneToast()
-    const [bookname, setBookname] = useState("")
-    const [author, setAuthor] = useState("")
-    const [isbnnumber, setIsbnumber] = useState("")
-    const [publisher, setPublisher] = useState("")
-    const [subject, setSubject] = useState("")
+  const [bookInfo, setBookInfo] = useState(defaultBookInfoState);
+  const toast = createStandaloneToast();
 
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setBookInfo((prev) => ({
+      ...prev,
+      [e.target.name]: value,
+    }));
+  };
 
-    const submit_entry = async (e)=>{
-        e.preventDefault()
+  const submit_entry = async (e) => {
+    e.preventDefault();
 
-        //// CONNECTION TO THE CUSTOM API GOES HERE USE AXIOS TO SENT POST REQ
-        let datatosend={
-            "authorname":author,
-            "booktitle":bookname,
-            "Subject":subject,
-            "Publisher":publisher,
-            "Isbn":isbnnumber
-        }
+    //// CONNECTION TO THE CUSTOM API GOES HERE USE AXIOS TO SENT POST REQ
+    let params = {
+      authorname: bookInfo.authorname,
+      booktitle: bookInfo.booktitle,
+      subject: bookInfo.Subject,
+      publisher: bookInfo.Publisher,
+      isbn: bookInfo.Isbn,
+      issued_data: "",
+    };
 
-        console.log(datatosend)
+    console.log(params);
 
-        setBookname('')
-        setAuthor('')
-        setIsbnumber('')
-        setPublisher('')
-        setSubject('')
-        // ACCESS THE CUSTOM API HERE AND SEND THE DATA
-        await axios.post(`http://localhost:8000/entry`, null, { params: {
-            authorname: author,
-            booktitle: bookname,
-            subject: subject,
-            publisher: publisher,
-            isbn: isbnnumber,
-            issued_data: ""
-          }})
-        .then(toast({
-            title: "Successfully verified data",
-            description: "Successfully Pushed data into our Database",
-            status: "success",
-            variant: "solid",
-            duration: 1500,
-            position: "top-right",
-            isClosable: false,
-          }))
-        .catch(
-            err =>
-            toast({
-                title: "Successfully verified data",
-                description: `Could not push data into our Database Error: ${err}`,
-                status: "error",
-                variant: "solid",
-                duration: 1500,
-                position: "top-right",
-                isClosable: false,
-              })
-        )
-      }
+    setBookInfo(defaultBookInfoState);
+    // ACCESS THE CUSTOM API HERE AND SEND THE DATA
+    await axios
+      .post(`http://localhost:8000/entry`, null, {
+        params: params,
+      })
+      .then(
+        toast({
+          title: `Successfully Added ${params.booktitle}`,
+          description: `Author: ${params.authorname}, ISBN: ${params.isbn}, Subject: ${params.subject}, Publisher: ${params.publisher}`,
+          status: "success",
+          variant: "solid",
+          duration: 3000,
+          position: "top-right",
+          isClosable: false,
+        })
+      )
+      .catch((err) =>
+        toast({
+          title: "Successfully verified data",
+          description: `Could not push data into our Database Error: ${err}`,
+          status: "error",
+          variant: "solid",
+          duration: 1500,
+          position: "top-right",
+          isClosable: false,
+        })
+      );
+  };
 
-    return(
-        <div>
-            <div className="entry">
-                <div className="entrycontainer">
-                    <div className="title">
-                        Add Entry Details</div>
-                        <form onSubmit={submit_entry}>
-                            <div className="entry-details">
-                                <div className="entry-box">
-                                    <span className="details">Book Name</span>
-                                    <input value={bookname} placeholder="Enter Book Name" onChange={(e)=>{setBookname(e.target.value)}}  required/>
-                                </div>
-                                <div className="entry-box">
-                                    <span className="details">Author</span>
-                                    <input value={author} placeholder="Enter Author Name" onChange={(e)=>{setAuthor(e.target.value)}}  required/>
-                                </div>
-                                <div className="entry-box">
-                                    <span className="details">Isbn Number</span>
-                                    <input value={isbnnumber} placeholder="Enter Isbn Number" onChange={(e)=>{setIsbnumber(e.target.value)}}  required/>
-                                </div>
-                                <div className="entry-box">
-                                    <span className="details">Publisher</span>
-                                    <input value={publisher} placeholder="Enter Publisher" onChange={(e)=>{setPublisher(e.target.value)}}  required/>
-                                </div>
-                                <div className="entry-box">
-                                    <span className="details">Subject</span>
-                                    <input value={subject} placeholder="Enter Publisher" onChange={(e)=>{setSubject(e.target.value)}}  required/>
-                                </div>
-                            </div>
-                            <div className="button">
-                                <input type="submit" value="Add Entry"/>
-                            </div>
-                        </form>
-
-                </div>
+  return (
+    <div>
+      <div className="entry">
+        <div className="entrycontainer">
+          <div className="title">Add Entry Details</div>
+          <form onSubmit={submit_entry}>
+            <div className="entry-details">
+              <div className="entry-box">
+                <span className="details">Book Name</span>
+                <input
+                  name={Object.keys(defaultBookInfoState)[2]}
+                  value={bookInfo.booktitle}
+                  placeholder="Enter Book Name"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="entry-box">
+                <span className="details">Author</span>
+                <input
+                  name={Object.keys(defaultBookInfoState)[1]}
+                  value={bookInfo.authorname}
+                  placeholder="Enter Author Name"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="entry-box">
+                <span className="details">Isbn Number</span>
+                <input
+                  name={Object.keys(defaultBookInfoState)[5]}
+                  value={bookInfo.Isbn}
+                  placeholder="Enter Isbn Number"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="entry-box">
+                <span className="details">Publisher</span>
+                <input
+                  name={Object.keys(defaultBookInfoState)[4]}
+                  value={bookInfo.Publisher}
+                  placeholder="Enter Publisher"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="entry-box">
+                <span className="details">Subject</span>
+                <input
+                  name={Object.keys(defaultBookInfoState)[3]}
+                  value={bookInfo.Subject}
+                  placeholder="Enter Publisher"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
             </div>
+            <div className="button">
+              <input type="submit" value="Add Entry" />
+            </div>
+          </form>
         </div>
-    )
-
+      </div>
+    </div>
+  );
 }
-
