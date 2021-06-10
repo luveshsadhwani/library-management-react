702f037dc0fc7833314fa87aee4c27ebfe1a70cf
Luvesh L Sadhwani <luveshsadhwani@gmail.com>
2021-06-11 01:56:18 +0800

    form validation for adding books form

--- src/components/navigator/AppNavigator.jsx
+++ src/components/navigator/AppNavigator.jsx
@@ -6,7 +6,7 @@ import Profile from "../../container/Profile";
 import Dashboard from "../../container/Dashboard";
 import Issues from "../../container/Issues";
 import Location from "../../container/Location";
-import AddEntry from "../../container/functionalitycomp/AddEntry";
+import AddEntryFormik from "../../container/functionalitycomp/AddEntryFormik"
 import EditBookFormik from "../Forms/EditBookFormik";
 import View from "../View/View";
 import IssueFormFormik from "../Forms/IssueFormFormik";
@@ -21,7 +21,7 @@ export default function AppNavigator() {
       <Route path={`${match}/dashboard`} component={Dashboard} exact />
       <Route path={`${match}/events`} component={Issues} exact />
       <Route path={`${match}/location`} component={Location} exact />
-      <Route path={`${match}/add`} component={AddEntry} exact />
+      <Route path={`${match}/add`} component={AddEntryFormik} exact />
       <Route path={`${match}/edit/:id`} component={EditBookFormik} exact />
       <Route path={`${match}/view/:isbn`} component={View} exact />
       <Route path={`${match}/issue/:id`} component={IssueFormFormik} exact />
---
+++ src/container/functionalitycomp/AddEntryFormik.jsx
@@ -0,0 +1,151 @@
+import React from "react";
+import axios from "axios";
+import "./addentry.css";
+import { createStandaloneToast } from "@chakra-ui/react";
+import { Formik, Form, Field, useField, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const TextField = ({ label, ...props }) => {
+  const [field] = useField(props);
+  return (
+    <div className="entry-box">
+      <label className="details" htmlFor={field.name}>
+        {label}
+      </label>
+      <Field name={field.name} {...props} />
+      <ErrorMessage
+        name={field.name}
+        render={(msg) => <div style={{ color: "red" }}>{msg}</div>}
+      />
+    </div>
+  );
+};
+
+// check if staff have a specific email, we can use regex to match this
+//check phone number format
+function AddPresentational(props) {
+
+  // Formik keeps tracks of form values, so we won't have useState here, take in the state from the parent. These will map out the keys and values for each textfield we have through the name prop
+  const { bookInfo } = props;
+
+  // Yup can validate for different types of data. Here we validate a string that matches a regex so that we don't get numbers. I've written it separately to make it neater. I allowed numbers for the booktitle, but not author, publisher or subject
+  const textValidation = Yup.string().matches(
+    /^[a-zA-Z. ]+$/,
+    "No numbers allowed"
+  );
+
+  // This is our validation schema, we can assign different validation types for each field. For ISBN, we are accepting hyphens too
+  const validate = Yup.object({
+    authorname: textValidation.required("Required"),
+    booktitle: Yup.string().required("Required"),
+    Isbn: Yup.string()
+      .matches(
+        /^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$/,
+        "ISBN must be 10 or 13 digits"
+      )
+      .required("Required"),
+    Publisher: textValidation.required("Required"),
+    Subject: textValidation.required("Required"),
+  });
+
+  const submit_entry = async (values, actions) => {
+    setTimeout(actions.resetForm, 1000) ;
+    const toast = createStandaloneToast();
+    //// CONNECTION TO THE CUSTOM API GOES HERE USE AXIOS TO SENT POST REQ
+    let params = {
+      booktitle: values.booktitle,
+      authorname: values.authorname,
+      publisher: values.Publisher,
+      subject: values.Subject,
+      isbn: values.Isbn,
+      issued_data: "",
+    };
+
+    // UPDATE WHERE ID
+    await axios
+      .post(`http://localhost:8000/entry`, null, {
+        params: params,
+      })
+      .then(() => {
+        toast({
+          title: `Successfully added ${values.booktitle}`,
+          description: `Author: ${values.authorname}, ISBN: ${values.Isbn}`,
+          status: "success",
+          variant: "solid",
+          duration: 1500,
+          position: "top-right",
+          isClosable: false,
+        });
+      })
+      .catch((err) => {
+        toast({
+          title: "Error Pushing Data",
+          description: `Error: ${err}`,
+          status: "error",
+          variant: "solid",
+          duration: 3000,
+          position: "top-right",
+          isClosable: false,
+        });
+      });
+  };
+
+  // Formik automatically tracks our input values and manages the onChange and onSubmit props
+  return (
+    <div className="entry">
+      <div className="entrycontainer">
+        <div className="title">Add Entry Details</div>
+        <Formik
+          initialValues={bookInfo}
+          onSubmit={submit_entry}
+          validationSchema={validate}
+        >
+          <Form>
+            <div className="entry-details">
+              <TextField
+                name="booktitle"
+                label="Book Title"
+                placeholder="Enter Book Title"
+              />
+              <TextField
+                name="authorname"
+                label="Author"
+                placeholder="Enter Author"
+              />
+              <TextField name="Isbn" label="ISBN" placeholder="Enter ISBN" />
+              <TextField
+                name="Publisher"
+                label="Publisher"
+                placeholder="Enter Publisher"
+              />
+              <TextField
+                name="Subject"
+                label="Subject"
+                placeholder="Enter Subject"
+              />
+            </div>
+            <div className="button">
+              <input type="submit" value="Add Entry" />
+            </div>
+          </Form>
+        </Formik>
+      </div>
+    </div>
+  );
+}
+
+export default function AddBookFormik() {
+  // I've run our get request separately here so the actual form will only be in charge of displaying and submitting ata
+
+  const defaultBookInfoState = {
+    id: "",
+    authorname: "",
+    booktitle: "",
+    Subject: "",
+    Publisher: "",
+    Isbn: "",
+    issued: "",
+  };
+
+  return <AddPresentational bookInfo={defaultBookInfoState} />;
+}
